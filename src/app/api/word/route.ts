b142a04e1a527dc420d3c1831c6089cd28deba5e
philip-meie <mixexfp@gmail.com>
2025-11-08 14:10:10 +0700

    Update

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -2,8 +2,10 @@ import { NextResponse } from "next/server";
 import { words } from "@/data/words";
 
 export async function GET() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const word = words[randomIndex];
-  console.log('Rose was here...');  
-  return NextResponse.json({ data: word });
+  //const randomIndex = Math.floor(Math.random() * words.length);
+  //const word = words[randomIndex];
+  const response = await fetch ('http://localhost:8000/api/word');
+  const data = await response.json();  
+  return NextResponse.json({data});
 }
+
