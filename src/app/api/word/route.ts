4ecff6264a3a0eb6806574893973372c643ae766
philip-meie <mixexfp@gmail.com>
2025-11-20 20:41:26 +0700

    Update route.ts

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -9,5 +9,5 @@ export async function GET() {
   console.log(response)
   const data = await response.json();
   console.log("Tae was here...")
-  return NextResponse.json({ data });
+  return NextResponse.json(data);
 }
